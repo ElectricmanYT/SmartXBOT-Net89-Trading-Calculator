b33ce1329a12d3b64a0c10ce1ae4af9faf00e8a0
Electricman <52600967+ElectricmanGamer@users.noreply.github.com>
2021-10-02 19:38:46 +0700

    Update tradingcalc.py

--- tradingcalc.py
+++ tradingcalc.py
@@ -13,33 +13,45 @@ USD = int(input("How many USD did you deposit: "))
 if (500 <= USD and 2500 > USD):
     print("Your bot price is 100 USD")
     Botfee = 100
+    time.sleep(1)
     print("Your profit share is 50%")
     Profitshare = 0.5
+    time.sleep(1)
 elif (2500 <= USD and 5000 > USD):
     print("Your bot price is 500 USD")
     Botfee = 500
+    time.sleep(1)
     print("Your profit share is 55%")
     Profitshare = 0.55
+    time.sleep(1)
 elif (5000 <= USD and 12500 > USD):
     print("Your bot price is 1,000 USD")
     Botfee = 1000
+    time.sleep(1)
     print("Your profit share is 60%")
     Profitshare = 0.6
+    time.sleep(1)
 elif (12500 <= USD and 25000 > USD):
     print("Your bot price is 2,500 USD")
     Botfee = 2500
+    time.sleep(1)
     print("Your profit share is 70%")
     Profitshare = 0.7
+    time.sleep(1)
 elif (25000 <= USD and 50000 > USD):
     print("Your bot price is 5,000 USD")
     Botfee = 5000
+    time.sleep(1)
     print("Your profit share is 80%")
     Profitshare = 0.8
+    time.sleep(1)
 elif (50000 <= USD):
     print("Your bot price is 10,000 USD")
     Botfee = 10000
+    time.sleep(1)
     print("Your profit share is 90%")
     Profitshare = 0.9
+    time.sleep(1)
 else:
     print("Your bot is not listed here.")
     value = True
@@ -56,12 +68,15 @@ Totalprofit = IDR * TD
 #Profit
 if (Whichprofit == 1):
     cut = int(input('Cut percentage (do not include "%"): '))
+    coma = "{:,.2f}".format(Totalprofit)
+    print("Your estimated total profit is Rp " + coma)
+    time.sleep(1)
     Profitcut = Totalprofit * cut/100
     s = "{:,.2f}".format(Profitcut)
-    print("Your profit is Rp " + s)
+    print("Your estimated cut profit is Rp " + s)
 elif (Whichprofit == 2):
     s = "{:,.2f}".format(Totalprofit)
-    print("Your profit is Rp " + s)
+    print("Your estimated total profit is Rp " + s)
 
 value = True
 while (value):
